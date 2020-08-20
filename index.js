92e1272d37540f61d9864e8cd94f483ba7b5502f
Said Furkan Dize <saidfurkandize@Saids-MacBook-Pro.local>
2020-08-20 23:13:53 +0300

    responsive fix

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 (() => {
   const cards = document.getElementsByClassName("card");
+  const outside = cards[0];
   const inside = cards[1];
   for (card of cards) {
     const windowHeight = window.innerHeight;
+    const windowWidth = window.innerWidth;
+    const isLimitedHeight = windowHeight > 600 && windowWidth < 600;
+    const maxHeight = isLimitedHeight
+      ? 600
+      : windowHeight;
+    const cardTop = isLimitedHeight
+      ? ((windowHeight - maxHeight) / 2) + maxHeight * 0.05
+      : maxHeight * 0.05;
+    const cardHeight = maxHeight * 0.9;
 
-    const cardMargin = windowHeight * 0.05;
-    const cardHeight = windowHeight * 0.9;
-    card.style.height =  cardHeight+ "px";
-    card.style.top = cardMargin + "px";
+    card.style.height =  cardHeight + "px";
+    card.style.top = cardTop + "px";
 
     card.onclick = () => {
-      inside.classList.toggle("hide");
+      outside.classList.toggle("hide");
+      inside.classList.toggle("show");
+    }
+    card.onmousedown = (event) => {
+      console.log("MOUSE DOWN", event)
+    }
+    card.onmouseup = (event) => {
+      console.log("MOUSE UP", event)
     }
   };
-})()
+})();

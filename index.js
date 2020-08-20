18e13d596f171d4ee002270d15741e411e7da977
Said Furkan Dize <saidfurkandize@Saids-MacBook-Pro.local>
2020-08-20 11:54:50 +0300

    fix for mobile statusbar

--- index.js
+++ index.js
@@ -2,6 +2,13 @@
   const cards = document.getElementsByClassName("card");
   const inside = cards[1];
   for (card of cards) {
+    const windowHeight = window.innerHeight;
+
+    const cardMargin = windowHeight * 0.1;
+    const cardHeight = windowHeight * 0.8;
+    card.style.height =  cardHeight+ "px";
+    card.style.top = cardMargin + "px";
+
     card.onclick = () => {
       inside.classList.toggle("hide");
     }

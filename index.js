389d5a80a2bfe534fe7ad1d6dab23fc0b5c94854
Said Furkan Dize <saidfurkandize@Saids-MacBook-Pro.local>
2020-08-21 09:27:40 +0300

    responsive fix

--- index.js
+++ index.js
@@ -2,18 +2,18 @@
   const cards = document.getElementsByClassName("card");
   const outside = cards[0];
   const inside = cards[1];
-  for (card of cards) {
-    const windowHeight = window.innerHeight;
-    const windowWidth = window.innerWidth;
-    const isLimitedHeight = windowHeight > 600 && windowWidth < 600;
-    const maxHeight = isLimitedHeight
-      ? 600
-      : windowHeight;
-    const cardTop = isLimitedHeight
-      ? ((windowHeight - maxHeight) / 2) + maxHeight * 0.05
-      : maxHeight * 0.05;
-    const cardHeight = maxHeight * 0.9;
+  const windowHeight = window.innerHeight;
+  const windowWidth = window.innerWidth;
+  const isLimitedHeight = windowHeight > 600 && windowWidth < 600;
+  const maxHeight = isLimitedHeight
+    ? 600
+    : windowHeight;
+  const cardTop = isLimitedHeight
+    ? ((windowHeight - maxHeight) / 2) + maxHeight * 0.05
+    : maxHeight * 0.05;
+  const cardHeight = maxHeight * 0.9;
 
+  for (card of cards) {
     card.style.height =  cardHeight + "px";
     card.style.top = cardTop + "px";
 
